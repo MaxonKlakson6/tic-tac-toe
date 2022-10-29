067a666fd3bac7036a308198dc8033d6809ee93a
Maxim <mihalenko.maxim21@gmail.com>
2022-10-29 13:05:38 +0300

    Added connection between server and client

--- server/index.js
+++ server/index.js
@@ -1,9 +1,21 @@
-const express = require('express');
-const http = require('http');
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
+
 const server = http.createServer(app);
+const webSocketServer = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+webSocketServer.on("connect", (socket) => {});
 
 server.listen(5000, () => {
-    console.log('server ran on port 3000');
-})
+  console.log("server ran on port 5000");
+});
